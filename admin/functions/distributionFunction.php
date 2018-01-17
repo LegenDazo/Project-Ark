a5aed3a9d1a48256c43d1bc4461e8232851421fe
mylenepepito <mylenedelimapepito@yahoo.com>
2018-01-17 22:49:12 +0800

    Fixed everything :)

---
+++ admin/functions/distributionFunction.php
@@ -0,0 +1,39 @@
+<?php
+
+class Functions
+{
+	public $con;
+
+	public function __construct()
+	{
+		$this->con = mysqli_connect("localhost", "root", "", "ark");
+	}
+
+		public function insertPackageDistribution($package_id, $household_id){
+			$sql = "INSERT INTO packagedistribution (package_id, household_id) VALUES ('".$package_id."','".$household_id."')";
+			$query = mysqli_query($this->con, $sql);
+			if ($query) {
+				return true;
+			} else {
+				echo mysqli_error($this->con);
+			}
+		}
+		
+}
+
+$Functions = new Functions;
+
+
+if(isset($_POST['received'])) {
+	$Functions->insertPackageDistribution($_POST["package_id"], $_POST["household_id"]);
+	header("location: ../reliefHousehold.php");
+/*	$resident_id = $_GET['resident_id'];
+	$evac_id = $_GET['evac_id'];
+
+		$population=$Functions->currentPopulation($evac_id);
+		$population++;
+		$Functions->updatePopulation($evac_id, $population);
+		$status++;
+		$Functions->insertAttendance($resident_id, $evac_id, $status); */
+}
+?>

f1bbb6aac0ba3f55afe8e367a2cf5ec35ee1bccd
mylenepepito <mylenedelimapepito@yahoo.com>
2018-01-18 11:31:28 +0800

    household & sql

--- ark.sql
+++ ark.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2018 at 11:21 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
+-- Generation Time: Jan 18, 2018 at 04:29 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -27,23 +29,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
+  `admin_id` varchar(25) NOT NULL,
   `fname` varchar(255) NOT NULL,
   `mname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
   `gender` varchar(6) NOT NULL,
   `age` int(11) NOT NULL,
-  `bday` date NOT NULL
+  `bday` date NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`username`, `password`, `fname`, `mname`, `lname`, `gender`, `age`, `bday`) VALUES
-('1', '', 'Jorge', 'Philip', 'Codilla', 'Male', 20, '1997-10-04'),
-('admin', 'admin', 'John Kent', 'Cabanez', 'Virtudazo', 'Male', 20, '2018-01-07');
+INSERT INTO `admin` (`admin_id`, `fname`, `mname`, `lname`, `gender`, `age`, `bday`, `user_id`) VALUES
+('1', 'Jorge', 'Philip', 'Codilla', 'Male', 20, '1997-10-04', 1);
 
 -- --------------------------------------------------------
 
@@ -94,7 +95,8 @@ CREATE TABLE `attendance` (
 INSERT INTO `attendance` (`attendance_id`, `date`, `resident_id`, `evac_id`, `status`) VALUES
 (7, '2018-01-10 12:03:56', 4, 2, 0),
 (8, '2018-01-14 06:47:42', 1, 2, 0),
-(9, '2018-01-14 06:48:56', 3, 3, 0);
+(9, '2018-01-14 06:48:56', 3, 3, 0),
+(15, '2018-01-17 13:58:44', 5, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -133,8 +135,7 @@ CREATE TABLE `disease` (
 --
 
 INSERT INTO `disease` (`disease_id`, `disease_name`) VALUES
-(2, 'colds'),
-(3, 'fever');
+(4, 'infection');
 
 -- --------------------------------------------------------
 
@@ -158,7 +159,8 @@ INSERT INTO `diseaseacquired` (`acquired_id`, `resident_id`, `disease_id`) VALUE
 (13, 2, 2),
 (14, 3, 3),
 (15, 4, 2),
-(16, 1, 2);
+(16, 1, 2),
+(18, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -183,7 +185,7 @@ CREATE TABLE `evacuationcenter` (
 --
 
 INSERT INTO `evacuationcenter` (`evac_id`, `location_name`, `population`, `capacity`, `latitude`, `longitude`, `brgy_id`, `house_no`, `street`) VALUES
-(2, 'Metrobank', 1, 100, 10.3692, 123.917, 7, 100, 'Gov. M. Cuenco Ave'),
+(2, 'Metrobank', 2, 100, 10.3692, 123.917, 7, 100, 'Gov. M. Cuenco Ave'),
 (3, 'San Isidro Parish School', 0, 200, 10.3691, 123.918, 7, 0, 'Gov. M. Cuenco Ave'),
 (4, 'FOTOJAYA STUDIO', 0, 50, 10.3692, 123.916, 7, 0, 'Gov. M. Cuenco Ave'),
 (5, 'Barangay Talamban Gymnasium', 0, 300, 10.3696, 123.917, 7, 0, 'Gov. M. Cuenco Ave'),
@@ -223,18 +225,12 @@ CREATE TABLE `item` (
   `package_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `itemdistribution`
+-- Dumping data for table `item`
 --
 
-CREATE TABLE `itemdistribution` (
-  `itemdist_id` int(11) NOT NULL,
-  `date_dist` date NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `resident_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `item` (`item_no`, `item_name`, `qty`, `item_type`, `sponsor_id`, `package_id`) VALUES
+(1, 'Neozep', 20, 'medicine', 2, 123);
 
 -- --------------------------------------------------------
 
@@ -244,10 +240,18 @@ CREATE TABLE `itemdistribution` (
 
 CREATE TABLE `packagedistribution` (
   `packdist_id` int(11) NOT NULL,
-  `date_dist` date NOT NULL,
+  `date_dist` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `package_id` int(11) NOT NULL,
   `household_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `packagedistribution`
+--
+
+INSERT INTO `packagedistribution` (`packdist_id`, `date_dist`, `package_id`, `household_id`) VALUES
+(5, '2018-01-17 14:46:27', 123, 1),
+(6, '2018-01-17 14:46:51', 123, 5);
 
 -- --------------------------------------------------------
 
@@ -261,6 +265,13 @@ CREATE TABLE `reliefoperation` (
   `evac_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `reliefoperation`
+--
+
+INSERT INTO `reliefoperation` (`operation_id`, `operation_name`, `evac_id`) VALUES
+(1, 'Sagip Kababayan', 3);
+
 -- --------------------------------------------------------
 
 --
@@ -272,6 +283,13 @@ CREATE TABLE `reliefpackage` (
   `package_name` varchar(255) NOT NULL,
   `operation_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reliefpackage`
+--
+
+INSERT INTO `reliefpackage` (`package_id`, `package_name`, `operation_id`) VALUES
+(123, 'medicine by dazo', 1);
 
 -- --------------------------------------------------------
 
@@ -305,9 +323,9 @@ INSERT INTO `resident` (`resident_id`, `fname`, `mname`, `lname`, `gender`, `bda
 (4, 'Abigail', 'Inoc', 'Velasquez', 'Female', '2018-01-10', 0, '', 789, 'Paknaan', '', 1),
 (5, '12', '12', '12', '12', '2018-12-31', 7, '', 12, '12', 'head', 1),
 (6, 'Max', 'Delante', 'Zuorba', 'Male', '2000-05-06', 7, '', 120, 'Nasipit, Talamban, Cebu', 'head', 5),
-(7, 'Monina', 'Garcia', 'So', 'Female', '1998-12-20', 7, '', 120, 'Nasipit, Talamban, Cebu', 'head''s spouse', 5),
+(7, 'Monina', 'Garcia', 'So', 'Female', '1998-12-20', 7, '', 120, 'Nasipit, Talamban, Cebu', 'head\'s spouse', 5),
 (8, 'jon', 'snow', 'kent', 'male', '2018-01-10', 7, '', 2342354, 'dfgdrgd', 'head', 6),
-(9, 'emilia', 'clarke', 'sdfse', 'female', '2018-01-01', 7, '', 2342354, 'dfgdrgd', 'head''s spouse', 6);
+(9, 'emilia', 'clarke', 'sdfse', 'female', '2018-01-01', 7, '', 2342354, 'dfgdrgd', 'head\'s spouse', 6);
 
 -- --------------------------------------------------------
 
@@ -331,10 +349,10 @@ CREATE TABLE `sms` (
 INSERT INTO `sms` (`sms_id`, `content`, `datesent`, `admin_id`, `status`, `username`) VALUES
 (3, 'PROJECTARK ADVISORY! Habang ang bagyo ay papalapit sa Pilipinas! Ang pilipinas naman ay papalayo ng papalayo sa bagyo!!', '2018-01-10 15:55:39', '', 1, 'LegenDazo'),
 (4, 'PROJECTARK ADVISORY! Habang ang bagyo ay papalapit sa Pilipinas! Ang pilipinas naman ay papalayo ng papalayo sa bagyo!!', '2018-01-10 15:55:43', '', 1, 'bacolod'),
-(7, 'PROJECTARK ADVISORY! ayaw nya ko biraha, basin ma hulog ta sa usa''g usa - dazo', '2018-01-10 16:17:44', '', 1, 'bacolod'),
-(8, 'PROJECTARK ADVISORY! ayaw nya ko biraha, basin ma hulog ta sa usa''g usa - dazo', '2018-01-10 16:17:46', '', 1, 'LegenDazo'),
-(9, 'PROJECTARK ADVISORY! "wa ka nalipong? ganina rman gud ka si''g tuyok sa ako ulo"', '2018-01-10 16:29:48', '', 1, 'bacolod'),
-(10, 'PROJECTARK ADVISORY! "wa ka nalipong? ganina rman gud ka si''g tuyok sa ako ulo"', '2018-01-10 16:29:49', '', 1, 'LegenDazo');
+(7, 'PROJECTARK ADVISORY! ayaw nya ko biraha, basin ma hulog ta sa usa\'g usa - dazo', '2018-01-10 16:17:44', '', 1, 'bacolod'),
+(8, 'PROJECTARK ADVISORY! ayaw nya ko biraha, basin ma hulog ta sa usa\'g usa - dazo', '2018-01-10 16:17:46', '', 1, 'LegenDazo'),
+(9, 'PROJECTARK ADVISORY! \"wa ka nalipong? ganina rman gud ka si\'g tuyok sa ako ulo\"', '2018-01-10 16:29:48', '', 1, 'bacolod'),
+(10, 'PROJECTARK ADVISORY! \"wa ka nalipong? ganina rman gud ka si\'g tuyok sa ako ulo\"', '2018-01-10 16:29:49', '', 1, 'LegenDazo');
 
 -- --------------------------------------------------------
 
@@ -363,6 +381,13 @@ CREATE TABLE `sponsor` (
   `contact_no` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `sponsor`
+--
+
+INSERT INTO `sponsor` (`sponsor_id`, `sponsor_name`, `sponsor_type`, `address`, `contact_no`) VALUES
+(2, 'Kent Dazo', 'Volunteer', 'Talamban, Cebu City', '09254853869');
+
 -- --------------------------------------------------------
 
 --
@@ -372,6 +397,7 @@ CREATE TABLE `sponsor` (
 CREATE TABLE `user` (
   `username` varchar(25) NOT NULL,
   `password` varchar(80) NOT NULL,
+  `user_type` varchar(25) NOT NULL,
   `fname` varchar(50) DEFAULT NULL,
   `mname` varchar(50) DEFAULT NULL,
   `lname` varchar(50) DEFAULT NULL,
@@ -383,10 +409,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `fname`, `mname`, `lname`, `bdate`, `contact_no`) VALUES
-('bacolod', 'bacolod', 'Mylene', 'Delima', 'Pepito', '2018-01-16', '639254853869'),
-('LegenDazo', 'legend', 'John Kent', 'Cabanez', 'Virtudazo', '2015-08-13', '639994738632'),
-('yeloverkill', 'yel', 'Ariel', 'Sarcon', 'Grayda', '1997-09-09', '0910291536');
+INSERT INTO `user` (`username`, `password`, `user_type`, `fname`, `mname`, `lname`, `bdate`, `contact_no`) VALUES
+('admin', 'admin', 'admin', 'Jorge', 'Philip', 'Codilla', '1997-10-04', '09451455958'),
+('bacolod', 'bacolod', 'resident', 'Mylene', 'Delima', 'Pepito', '2018-01-16', '639254853869'),
+('LegenDazo', 'legend', 'user', 'John Kent', 'Cabanez', 'Virtudazo', '2015-08-13', '639994738632');
 
 --
 -- Indexes for dumped tables
@@ -396,7 +422,7 @@ INSERT INTO `user` (`username`, `password`, `fname`, `mname`, `lname`, `bdate`, 
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `announcement`
@@ -454,15 +480,10 @@ ALTER TABLE `item`
   ADD KEY `package_id` (`package_id`);
 
 --
--- Indexes for table `itemdistribution`
---
-ALTER TABLE `itemdistribution`
-  ADD KEY `resident_id` (`resident_id`);
-
---
 -- Indexes for table `packagedistribution`
 --
 ALTER TABLE `packagedistribution`
+  ADD PRIMARY KEY (`packdist_id`),
   ADD KEY `package_id` (`package_id`),
   ADD KEY `household_id` (`household_id`);
 
@@ -521,66 +542,85 @@ ALTER TABLE `user`
 --
 ALTER TABLE `announcement`
   MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
 --
 -- AUTO_INCREMENT for table `barangay`
 --
 ALTER TABLE `barangay`
   MODIFY `brgy_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `disease`
 --
 ALTER TABLE `disease`
-  MODIFY `disease_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `disease_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `diseaseacquired`
 --
 ALTER TABLE `diseaseacquired`
-  MODIFY `acquired_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `acquired_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
 --
 -- AUTO_INCREMENT for table `evacuationcenter`
 --
 ALTER TABLE `evacuationcenter`
   MODIFY `evac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `household`
 --
 ALTER TABLE `household`
   MODIFY `household_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_no` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `item_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `packagedistribution`
+--
+ALTER TABLE `packagedistribution`
+  MODIFY `packdist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `reliefoperation`
 --
 ALTER TABLE `reliefoperation`
-  MODIFY `operation_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `operation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `resident`
 --
 ALTER TABLE `resident`
   MODIFY `resident_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `sms`
 --
 ALTER TABLE `sms`
   MODIFY `sms_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `smschecker`
 --
 ALTER TABLE `smschecker`
   MODIFY `checker_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `sponsor`
 --
 ALTER TABLE `sponsor`
-  MODIFY `sponsor_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sponsor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- Constraints for dumped tables
 --
@@ -589,7 +629,7 @@ ALTER TABLE `sponsor`
 -- Constraints for table `announcement`
 --
 ALTER TABLE `announcement`
-  ADD CONSTRAINT `announcement_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`username`);
+  ADD CONSTRAINT `announcement_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
 
 --
 -- Constraints for table `attendance`
@@ -610,12 +650,6 @@ ALTER TABLE `evacuationcenter`
 ALTER TABLE `item`
   ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`sponsor_id`) REFERENCES `sponsor` (`sponsor_id`),
   ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`package_id`) REFERENCES `reliefpackage` (`package_id`);
-
---
--- Constraints for table `itemdistribution`
---
-ALTER TABLE `itemdistribution`
-  ADD CONSTRAINT `itemdistribution_ibfk_1` FOREIGN KEY (`resident_id`) REFERENCES `resident` (`resident_id`);
 
 --
 -- Constraints for table `packagedistribution`
@@ -641,6 +675,7 @@ ALTER TABLE `resident`
 --
 ALTER TABLE `sms`
   ADD CONSTRAINT `sms_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

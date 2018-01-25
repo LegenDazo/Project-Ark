62fa234e13d789bcfbd3f2887e8aea1dec6906bf
av30 <abigail.velasquez77@gmail.com>
2018-01-26 03:27:40 +0800

    newannouncement

---
+++ admin/functions/newannounceFunctions.php
@@ -0,0 +1,112 @@
+<?php
+
+//include 'functions/retrieveEvacuationCenterFunction.php';
+
+	Class Functions
+	{
+		public $conn;
+		public function __construct()
+		{
+			$this->conn = mysqli_connect("localhost", "root", "", "ark");
+		}
+
+		public function insertAnnounce($admin_id, $an_what)
+		{
+			$sql = "INSERT INTO newannouncement (admin_id, an_what) VALUES ('".$admin_id."','".$an_what."')";
+			$query = mysqli_query($this->conn, $sql);
+			if ($query) {
+				return true;
+			} else {
+				echo mysqli_error($this->conn);
+			}
+		}
+
+		public function retrieveAnnounceItems($announce_id)
+		{
+			$sql = "SELECT * FROM newannouncement WHERE announce_id = '".$announce_id."'";
+			$query = mysqli_query($this->conn, $sql);
+			$itemArray = array();
+			while ($row = mysqli_fetch_assoc($query)) {
+				$itemArray[] = $row;
+			}
+			return $itemArray;
+		}
+
+		public function retrieveAnnounceData()
+		{
+			$sql = "SELECT * FROM newannouncement";
+			$query = mysqli_query($this->conn, $sql);
+			$itemArray = array();
+			while ($row = mysqli_fetch_assoc($query)) {
+				$itemArray[] = $row;
+			}
+			return $itemArray;
+		}
+
+		public function updateAnnounce($announce_id, $an_what)
+		{
+			$sql = "UPDATE newannouncement SET announce_id='".$announce_id."' WHERE announce_id='".$announce_id."'";
+			$query = mysqli_query($this->conn, $sql);
+			if ($query) {
+				return true;
+			} else {
+				echo mysqli_error($this->conn);
+			}
+		}
+
+		public function deleteAnnounce($announce_id)
+		{
+			$sql = "DELETE FROM newannouncement WHERE announce_id='".$announce_id."'";
+			$query = mysqli_query($this->conn, $sql);
+			if ($query) {
+				return true;
+			} else {
+				echo mysqli_error($this->conn);
+			}
+		}
+
+		public function retrieveAdminInfo($username)
+		{
+			$sql = "SELECT * FROM admin WHERE username ='".$username."'";
+			$itemArray = array();
+			$query = mysqli_query($this->conn, $sql);
+			while ($row = mysqli_fetch_assoc($query)) {
+				$itemArray[] = $row;
+			}
+			return $itemArray;
+		}
+
+	}
+
+	$func = new Functions;
+
+	if (isset($_POST['submitannounce'])) {
+		$admin_id = 1;
+		$an_what = mysqli_real_escape_string($func->conn, $_POST['an_what']);
+
+		
+		if ($func->insertAnnounce($admin_id, $an_what)) {
+			header("location:../newannouncement.php?inserted=1");
+		}
+	}
+
+	if (isset($_POST['updateannounce'])) {
+
+		$announce_id = mysqli_real_escape_string($func->conn, $_GET['announce_id']);
+		$an_what = mysqli_real_escape_string($func->conn, $_POST['an_what']);
+		
+				
+		if ($func->updateAnnounce($announce_id, $an_what)) {
+			header("location:../viewAnnounceDetails.php?updated=1&announce_id=$announce_id");
+		}
+	}
+
+	if (isset($_GET['deleteannounce'])) {
+		$announce_id = mysqli_real_escape_string($func->conn, $_GET['announce_id']);
+
+		
+			if ($func->deleteAnnounce($announce_id)) {
+			header("location:newannouncement.php?deleted=1");
+		}		
+	}
+?>
---
+++ admin/newaddAnnounce.php
@@ -0,0 +1,91 @@
+<?php session_start();
+  if ($_SESSION['username'] == "" && $_SESSION['type'] == "" || $_SESSION['type'] == "normal") {
+      header("location:../logout.php");
+  }
+?>
+<!DOCTYPE html>
+<?php include 'functions/newannounceFunctions.php'; 
+      include 'functions/retrieveEvacuationCenterFunction.php';
+?>
+<html lang="en">
+<head>
+  <title>Project Ark</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../style.css">
+  <link rel="stylesheet" href="../materialize/icons.css">
+  <link rel="stylesheet" type="text/css" href="../datatables/datatables.css">
+  <link rel="stylesheet" type="text/css" href="../datatables/datatables-bootstrap.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-light bg-faded">
+    <img src="../images/ARK1.png">
+    <a href="../logout.php" style="color: white">Log Out</a>
+    </nav>
+
+    <div class="container-fluid"><!--START OF MAIN CONTAINER-->
+      <div class="row"><!--start of row-->
+
+       <?php include '../adminNavbar.php'; ?>
+        
+          <?php
+            if(isset($_GET['announce_id'])) {
+               $announce_id = $_GET['announce_id']; 
+          } 
+          ?>
+
+
+            <div class="col-md-9"><!-- START of RIGHT COLUMN-->
+              <div class="card" style="margin-top: 25px;"><!--START OF RIGHTCARD-->                
+                  <div class="container" style="margin-top: 25px; margin-bottom: 25px;">
+                          <center><h5>Announcement Details</h5></center>
+                          <div class="container" style="margin-top: 5%">
+                              
+                                  <form method="POST" action="functions/newannounceFunctions.php">
+                                     <div class="panel-body">  
+                                            <div class="form-group col-md-12">
+                                              <label for="an_what"><h6>Input Announcements Below:</h6></label>
+                                              <textarea class="form-control" rows="9" id="an_what" name="an_what" required></textarea>                        
+                                            </div>
+                                        
+ 
+    
+
+                                    </div>  <!--End of .panel-body-->
+                                    <br>     
+                                    
+                                        <div class="panel-footer">
+                                          <div class="text-right">
+                                          <a href="newannouncement.php" class="btn btn-warning">Cancel</a>  
+                                          <button type="submit" class="btn btn-primary" name="submitannounce">Post</button>
+                                          </div>
+                                        </div>
+
+                                  </form>
+
+                              
+                          </div>
+                  </div>
+              </div><!--END OF RIGHTCARD--> 
+            </div><!-- END of RIGHT COLUMN-->
+     
+      </div><!--end of row-->
+    </div><!--END OF MAIN CONTIANER-->
+
+
+
+
+
+
+<script src="../js/jquery.min.js"></script>
+<script src="../bootstrap/js/bootstrap.js"></script>
+<script src="../bootstrap/js/bootstrap_alpha6.min.js"></script>
+
+
+
+</body>
+</html>
